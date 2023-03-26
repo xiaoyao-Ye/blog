ac3ef714d0c5f61a01b1df10999d63d23a48e5b9
xiaoyao-Ye <yqcyecong@163.com>
2023-03-26 10:44:01 +0800

    docs: ✏️ LeetCode every day

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -143,6 +143,7 @@ function sidebarLeetCode() {
         { text: '[简单] 09.回文数', link: '/leetCode/09.md' },
         { text: '[中等] 36.有效的数独', link: '/leetCode/36.md' },
         { text: '[简单] 70.爬楼梯', link: '/leetCode/70.md' },
+        { text: '[简单] 125.验证回文串', link: '/leetCode/125.md' },
         { text: '[简单] 242.有效的字母异位词', link: '/leetCode/242.md' },
         { text: '[简单] 387.字符串中的第一个唯一字符', link: '/leetCode/387.md' },
       ],
