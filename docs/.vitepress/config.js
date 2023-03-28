e1d6eb98350cdb468b0c901c5da7a65fd59b967d
xiaoyao-Ye <yqcyecong@163.com>
2023-03-28 15:36:59 +0800

    docs: ✏️ LeetCode every day

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -143,6 +143,7 @@ function sidebarLeetCode() {
         { text: '[简单] 09.回文数', link: '/leetCode/09.md' },
         { text: '[简单] 20.有效的括号', link: '/leetCode/20.md' },
         { text: '[中等] 36.有效的数独', link: '/leetCode/36.md' },
+        { text: '[简单] 59.最后一个单词的长度', link: '/leetCode/59.md' },
         { text: '[简单] 70.爬楼梯', link: '/leetCode/70.md' },
         { text: '[简单] 125.验证回文串', link: '/leetCode/125.md' },
         { text: '[简单] 242.有效的字母异位词', link: '/leetCode/242.md' },
