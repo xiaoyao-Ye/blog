1ea77f18ab0bfc047e270fd0cd48c06c49dc8f8b
Ghosteye <yqcyecong@163.com>
2023-07-20 18:09:46 +0800

    docs: ✏️ Update docs

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vitepress'
+import { navBar, sidebar } from './router'
 
 export default defineConfig({
   // srcDir: './src',
@@ -31,14 +32,8 @@ export default defineConfig({
       pattern: 'https://github.com/xiaoyao-Ye/blog/edit/main/docs/:path',
       text: '在 GitHub 上编辑此页面',
     },
-    nav: nav(),
-    sidebar: {
-      '/handbook/': sidebarHandbook(),
-      '/leetCode/': sidebarLeetCode(),
-      '/interview/': sidebarInterview(),
-      '/other/': sidebarOther(),
-      '/Ghosteye/': sidebarGhosteye(),
-    },
+    nav: navBar,
+    sidebar,
     socialLinks: [{ icon: 'github', link: 'https://github.com/xiaoyao-Ye' }],
     footer: {
       message: 'Released under the MIT License.',
@@ -99,107 +94,3 @@ export default defineConfig({
     // },
   },
 })
-
-function nav() {
-  return [
-    { text: 'handbook', activeMatch: '/handbook/', link: '/handbook/' },
-    { text: 'LeetCode', activeMatch: '/leetCode/', link: '/leetCode/01.md' },
-    { text: 'interview', activeMatch: '/interview/', link: '/interview/' },
-    { text: 'other', activeMatch: '/other/', link: '/other/' },
-    { text: 'case', activeMatch: '/case/', link: '/case/' },
-    { text: 'Ghosteye', activeMatch: '/Ghosteye/', link: '/Ghosteye/initapi' },
-  ]
-}
-function sidebarHandbook() {
-  return [
-    {
-      text: '速查手册',
-      items: [
-        { text: '代码规范', link: '/handbook/coding-specification' },
-        { text: 'settings', link: '/handbook/settings' },
-        { text: 'Vue', link: '/handbook/vue' },
-        { text: 'Css', link: '/handbook/css' },
-        { text: 'JavaScript', link: '/handbook/JavaScript' },
-        { text: 'NodeJS', link: '/handbook/node' },
-        { text: 'mpvue', link: '/handbook/mpvue' },
-        { text: 'other', link: '/handbook/other' },
-      ],
-    },
-    {
-      text: '工程化',
-      items: [
-        { text: 'Git', link: '/handbook/Git' },
-        { text: 'npm', link: '/handbook/npm' },
-        { text: 'webpack', link: '/handbook/webpack' },
-      ],
-    },
-  ]
-}
-function sidebarLeetCode() {
-  return [
-    {
-      text: 'LeetCode',
-      items: [
-        { text: '[简单] 01.两数之和', link: '/leetCode/01.md' },
-        { text: '[中等] 03.无重复字符的最长子串', link: '/leetCode/03.md' },
-        { text: '[中等] 07.整数反转', link: '/leetCode/07.md' },
-        { text: '[简单] 09.回文数', link: '/leetCode/09.md' },
-        { text: '[简单] 14.最长公共前缀', link: '/leetCode/14.md' },
-        { text: '[简单] 20.有效的括号', link: '/leetCode/20.md' },
-        { text: '[简单] 27.移除元素', link: '/leetCode/27.md' },
-        { text: '[中等] 36.有效的数独', link: '/leetCode/36.md' },
-        { text: '[中等] 45.跳跃游戏 II', link: '/leetCode/45.md' },
-        { text: '[简单] 59.最后一个单词的长度', link: '/leetCode/59.md' },
-        { text: '[简单] 66.加一', link: '/leetCode/66.md' },
-        { text: '[简单] 70.爬楼梯', link: '/leetCode/70.md' },
-        { text: '[简单] 121.买卖股票的最佳时机', link: '/leetCode/121.md' },
-        { text: '[简单] 125.验证回文串', link: '/leetCode/125.md' },
-        { text: '[简单] 219.存在重复元素 II', link: '/leetCode/219.md' },
-        { text: '[简单] 242.有效的字母异位词', link: '/leetCode/242.md' },
-        { text: '[简单] 387.字符串中的第一个唯一字符', link: '/leetCode/387.md' },
-      ],
-    },
-  ]
-}
-function sidebarInterview() {
-  return [
-    {
-      text: '八股文',
-      items: [
-        { text: 'prepare', link: '/interview/prepare' },
-        { text: '工程化', link: '/interview/engineering' },
-      ],
-    },
-  ]
-}
-function sidebarOther() {
-  return [
-    {
-      text: '其他',
-      items: [
-        // { text: '书籍', link: '/other/books' },
-        { text: '原生应用', link: '/other/RN_Flutter_Weex' },
-        { text: 'Python', link: '/other/Python' },
-        { text: 'error', link: '/other/error' },
-        { text: 'docker', link: '/other/docker' },
-        { text: 'vscode', link: '/other/vscode' },
-        { text: 'quotations', link: '/other/quotations' },
-        { text: 'test', link: '/other/test' },
-        { text: 'mini', link: '/other/mini' },
-        { text: 'install', link: '/other/install' },
-      ],
-    },
-  ]
-}
-
-function sidebarGhosteye() {
-  return [
-    {
-      text: '个人项目使用介绍',
-      items: [
-        // { text: 'create-xm', link: '/Ghosteye/create-xm' },
-        { text: 'initapi', link: '/Ghosteye/initapi' },
-      ],
-    },
-  ]
-}
