186789f757163c8b3eb7700a5a81668df65e04d6
Ghosteye <yqcyecong@163.com>
2023-07-22 10:02:32 +0800

    style: 💄 Optimize

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,104 @@
+import { defineConfig } from "vitepress";
+import { navBar, sidebar } from "./router";
+
+export default defineConfig({
+  head: [
+    ["link", { rel: "icon", href: "/blog/favicon.ico" }],
+    ["meta", { name: "author", content: "Ghosteye" }],
+  ],
+  // srcDir: './src',
+  base: "/blog/",
+  cleanUrls: true,
+  title: "Ghosteye",
+  titleTemplate: false,
+  srcDir: "../docs",
+  srcExclude: ["**/README.md", "**/TODO.md"],
+  // titleTemplate: '不要让时代的悲哀成为你的悲哀!',
+  // description: '当灾难来临时, 精神意志是人类的第一序列武器.',
+  description: "Do you have any output? Do it now!",
+  // 暗黑模式
+  appearance: "dark",
+  // 当设置为 时true，VitePress 不会因为死链接而导致构建失败。
+  ignoreDeadLinks: true,
+  // 使用 git commit 获取时间戳。此选项启用默认主题以显示页面的最后更新时间。
+  lastUpdated: true,
+  // 配置 Markdown 解析器选项。VitePress 使用Markdown-it作为解析器，使用Shiki来突出语言语法。在此选项中，您可以传递各种 Markdown 相关选项以满足您的需求。
+  markdown: {
+    // headers: {
+    //   level: [0, 0]
+    // }
+    // theme: 'material-palenight',
+    // lineNumbers: true
+  },
+  themeConfig: {
+    logo: "/img/book.png",
+    siteTitle: "Ghosteye",
+    outlineTitle: "在本页面",
+    outline: [2, 6],
+    lastUpdatedText: "最近更新时间",
+    editLink: {
+      pattern: "https://github.com/xiaoyao-Ye/blog/edit/main/docs/:path",
+      text: "在 GitHub 上编辑此页面",
+    },
+    nav: navBar,
+    sidebar,
+    socialLinks: [{ icon: "github", link: "https://github.com/xiaoyao-Ye" }],
+    footer: {
+      message: "Released under the MIT License.",
+      copyright: "Copyright © 2019-present Ghosteye",
+    },
+    algolia: {
+      appId: "EJYW8N5YY2",
+      apiKey: "b2a060fd4e6b6b5b249c13b9056c89f0",
+      indexName: "xiaoyao-yeio",
+      // locales: {
+      //   zh: {
+      //     placeholder: '搜索文档',
+      //     translations: {
+      //       button: {
+      //         buttonText: '搜索文档',
+      //         buttonAriaLabel: '搜索文档',
+      //       },
+      //       modal: {
+      //         searchBox: {
+      //           resetButtonTitle: '清除查询条件',
+      //           resetButtonAriaLabel: '清除查询条件',
+      //           cancelButtonText: '取消',
+      //           cancelButtonAriaLabel: '取消',
+      //         },
+      //         startScreen: {
+      //           recentSearchesTitle: '搜索历史',
+      //           noRecentSearchesText: '没有搜索历史',
+      //           saveRecentSearchButtonTitle: '保存至搜索历史',
+      //           removeRecentSearchButtonTitle: '从搜索历史中移除',
+      //           favoriteSearchesTitle: '收藏',
+      //           removeFavoriteSearchButtonTitle: '从收藏中移除',
+      //         },
+      //         errorScreen: {
+      //           titleText: '无法获取结果',
+      //           helpText: '你可能需要检查你的网络连接',
+      //         },
+      //         footer: {
+      //           selectText: '选择',
+      //           navigateText: '切换',
+      //           closeText: '关闭',
+      //           searchByText: '搜索提供者',
+      //         },
+      //         noResultsScreen: {
+      //           noResultsText: '无法找到相关结果',
+      //           suggestedQueryText: '你可以尝试查询',
+      //           reportMissingResultsText: '你认为该查询应该有结果？',
+      //           reportMissingResultsLinkText: '点击反馈',
+      //         },
+      //       },
+      //     },
+      //   },
+      // },
+    },
+    // 广告
+    // carbonAds: {
+    //   code: 'your-carbon-code',
+    //   placement: 'your-carbon-placement',
+    // },
+  },
+});
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,70 +1,64 @@
-import Theme from 'vitepress/theme'
+import Theme from "vitepress/theme";
 // import '../style/vars.css'
-import '../style/main.css'
-import './rainbow.css'
-import './vars.css'
-import './overrides.css'
+import "../style/main.css";
+import "./rainbow.css";
+import "./vars.css";
+import "./overrides.css";
 
-import { h, watch } from 'vue'
-import BG from '../../components/BG.vue'
-import Comment from '../../components/Comment.vue'
+import { h, watch } from "vue";
+import BG from "../../components/BG.vue";
+import Comment from "../../components/Comment.vue";
 
-let homePageStyle: HTMLStyleElement | undefined
+let homePageStyle: HTMLStyleElement | undefined;
 
 export default {
   ...Theme,
   Layout: () => {
     return h(Theme.Layout, null, {
-      'home-features-after': () => h(BG),
-      'doc-before': () => h(BG),
-      'doc-after': () => h(Comment),
+      "home-features-after": () => h(BG),
+      "doc-before": () => h(BG),
+      "doc-after": () => h(Comment),
       // 文章右边 sidebar 内容之后
       // 'aside-outline-after': () => h(BG)
-    })
+    });
   },
   enhanceApp({ router }) {
     // 可以用来判断是否在浏览器环境下
     // import { inBrowser } from 'vitepress'
     // if (inBrowser) {}
-    if (typeof window === 'undefined') return
-
-    const theme = window.localStorage.getItem('vitepress-theme-appearance')
-    if (theme === null) {
-      window.localStorage.setItem('vitepress-theme-appearance', 'dark')
-      window.location.reload()
-    }
+    if (typeof window === "undefined") return;
 
     watch(
       () => router.route.data.relativePath,
-      () => updateHomePageStyle(location.pathname === '/'),
+      () => updateHomePageStyle(location.pathname === "/"),
       { immediate: true },
-    )
+    );
   },
-}
+};
 
-if (typeof window !== 'undefined') {
+if (typeof window !== "undefined") {
   // detect browser, add to class for conditional styling
-  const browser = navigator.userAgent.toLowerCase()
-  if (browser.includes('chrome')) document.documentElement.classList.add('browser-chrome')
-  else if (browser.includes('firefox')) document.documentElement.classList.add('browser-firefox')
-  else if (browser.includes('safari')) document.documentElement.classList.add('browser-safari')
+  const browser = navigator.userAgent.toLowerCase();
+  if (browser.includes("chrome")) document.documentElement.classList.add("browser-chrome");
+  else if (browser.includes("firefox")) document.documentElement.classList.add("browser-firefox");
+  else if (browser.includes("safari")) document.documentElement.classList.add("browser-safari");
 }
 
 // Speed up the rainbow animation on home page
 function updateHomePageStyle(value: boolean) {
   if (value) {
-    if (homePageStyle) return
+    if (homePageStyle) return;
 
-    homePageStyle = document.createElement('style')
+    homePageStyle = document.createElement("style");
     homePageStyle.innerHTML = `
     :root {
       animation: rainbow 12s linear infinite;
-    }`
-    document.body.appendChild(homePageStyle)
+    }`;
+    document.body.appendChild(homePageStyle);
   } else {
-    if (!homePageStyle) return
+    if (!homePageStyle) return;
 
-    homePageStyle.remove()
-    homePageStyle = undefined
+    homePageStyle.remove();
+    homePageStyle = undefined;
   }
 }
